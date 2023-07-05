b8147c10999b86054789a58873b7ccb921d90634
Mirali Mirzayev <miralimirzayev@Miralis-MacBook-Pro.local>
2023-07-05 18:32:28 -0400

    Same Change as before

--- pages/cnn_3.py
+++ pages/cnn_3.py
@@ -10,7 +10,7 @@ import base64
 from streamlit.components.v1 import html 
 import tensorflow as tf 
 from tensorflow.keras.applications.imagenet_utils import decode_predictions
-import cv2 
+# import cv2 
 from PIL import Image, ImageOps 
 import numpy as np
 
