bba48efd8f4500b01b30a3d8852e39e6dcfd22c0
Mirali Mirzayev <miralimirzayev@Miralis-MacBook-Pro.local>
2023-07-05 18:33:48 -0400

    path change

--- Streamlit_Dev/pages/cnn_2.py
+++ Streamlit_Dev/pages/cnn_2.py
@@ -16,7 +16,7 @@ import numpy as np
 
 # cnn_model = pickle.load(open("/Users/miralimirzayev/Desktop/McCombs_Bootcamp/cohortDec17/projects/Streamlit_Dev/CNN_Model/saved_model.pb","rb"))
 # cnn_model = tf.keras.models.load_model("/Users/miralimirzayev/Desktop/McCombs_Bootcamp/cohortDec17/projects/Streamlit_Dev/CNN_Model/saved_model.pb")
-cnn_model = tf.keras.models.load_model("/Users/miralimirzayev/Desktop/McCombs_Bootcamp/cohortDec17/streamlit_apps/models/cnn_3_model.hdf5")
+cnn_model = tf.keras.models.load_model("/Users/miralimirzayev/Desktop/McCombs_Bootcamp/cohortDec17/streamlit_app/models/cnn_3_model.hdf5")
 
 st.set_page_config(
 	page_title = "CNN - Project #7 - Image Predictor",
