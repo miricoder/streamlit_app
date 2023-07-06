e55f3808f9e9fdd8f04c86c0caa89aaa183e1e5f
Mirali Mirzayev <miralimirzayev@Miralis-MacBook-Pro.local>
2023-07-05 21:11:38 -0400

    1a

--- pages/cnn_3.py
+++ pages/cnn_3.py
@@ -73,21 +73,21 @@ def get_base64_of_bin_file(png_file):
     image_width="60%",
     image_height="",
 ):
-    binary_string = get_base64_of_bin_file(png_file)
-    return """
-            <style>
-                [data-testid="stSidebarNav"] {
-                    background-image: url("data:image/png;base64,%s");
-                    background-repeat: no-repeat;
-                    background-position: %s;
-                    margin-top: %s;
-                    background-size: %s %s;
-                }
-            </style>
-            """ % (
-        binary_string,
-        background_position,
-        margin_top,
-        image_width,
-        image_height,
-    )
+binary_string = get_base64_of_bin_file(png_file)
+return """
+        <style>
+            [data-testid="stSidebarNav"] {
+                background-image: url("data:image/png;base64,%s");
+                background-repeat: no-repeat;
+                background-position: %s;
+                margin-top: %s;
+                background-size: %s %s;
+            }
+        </style>
+        """ % (
+    binary_string,
+    background_position,
+    margin_top,
+    image_width,
+    image_height,
+)
