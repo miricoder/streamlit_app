1d35830091f9e8c006cfa7d2b726515b002047d6
Mirali Mirzayev <miralimirzayev@Miralis-MacBook-Pro.local>
2023-07-05 21:14:01 -0400

    2a

--- pages/cnn_3.py
+++ pages/cnn_3.py
@@ -66,28 +66,39 @@ def get_base64_of_bin_file(png_file):
         data = f.read()
     return base64.b64encode(data).decode()
 
-    def build_markup_for_logo(
+
+def build_markup_for_logo(
     png_file,
     background_position="50% 10%",
     margin_top="10%",
     image_width="60%",
     image_height="",
 ):
-binary_string = get_base64_of_bin_file(png_file)
-return """
-        <style>
-            [data-testid="stSidebarNav"] {
-                background-image: url("data:image/png;base64,%s");
-                background-repeat: no-repeat;
-                background-position: %s;
-                margin-top: %s;
-                background-size: %s %s;
-            }
-        </style>
-        """ % (
-    binary_string,
-    background_position,
-    margin_top,
-    image_width,
-    image_height,
-)
+    binary_string = get_base64_of_bin_file(png_file)
+    return """
+            <style>
+                [data-testid="stSidebarNav"] {
+                    background-image: url("data:image/png;base64,%s");
+                    background-repeat: no-repeat;
+                    background-position: %s;
+                    margin-top: %s;
+                    background-size: %s %s;
+                }
+            </style>
+            """ % (
+        binary_string,
+        background_position,
+        margin_top,
+        image_width,
+        image_height,
+    )
+    
+
+
+
+def add_logo(png_file):
+    logo_markup = build_markup_for_logo(png_file)
+    st.markdown(
+        logo_markup,
+        unsafe_allow_html=True,
+    )
